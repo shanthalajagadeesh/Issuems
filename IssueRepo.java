7561ed3e11afed20f1b251633b8a582cc57a5bd7
shanthalajagadeesh <151831826+shanthalajagadeesh@users.noreply.github.com>
2024-10-20 13:03:42 +0530

    Delete IssueRepo.java

--- IssueRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.issuems.Repo;
-
-import com.example.issuems.Domain.Issue;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IssueRepo extends JpaRepository<Issue,String> {
-}
